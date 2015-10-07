d2aa9fb6f845262360b6d9b0085e9d19cf79096c
raphii97 <raphi.monteiro@gmail.com>
2015-10-08 01:01:19 +0200

    First commit

--- konami.js
+++ konami.js
@@ -1,61 +1,42 @@
-function KonamiCode(win_func){
-	var self = this;
-	//storing all the inputs
-	this.input = [];
+function Konami(surprise, sequence){
 
-	//the correct sequence of inputs (up: 38, down: 40, left: 37, right: 39, b: 66, a: 65, enter: 13)
-	this.ok = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];	//DA KONAMI CODE
+	//custom code sequence or default Konami code
+	var code = sequence || [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+	var code_length = code.length - 1;
 
-	//function that compares both arrays
-	this.check = function(a, b){
-		//var for counting the numbers of correct sequences
-		var c = 0;
+	//counter to iterate over code array
+	var counter = 0;
 
-		//looping trough the array
-		for (var i in a) {
-			if (a[i] === b[c]) {
-				//if all values are the same return true
-				if (c === (b.length - 1)) {
+	//checking if input corresponds to sequence
+	var check = function(input){
 
-					//reset input array
-					a.length = 0;
+		if(code[counter] == input.keyCode){
 
-					return true;
-				}
-				c++;
+			//if sequence completed
+			if(counter == code_length){
+				//reset counter
+				counter = 0;
+
+				//launching the custom function
+				surprise();
+			}else{
+				//go to next keycode in code array
+				counter++;
 			}
-			//else input sequence does not correspond to konami sequence
-			else{c = 0;}
+			
+		}else{
+			//reset counter
+			counter = 0;
 		}
 
-		//if array to big with incorrect sequence
-		/*
-		if(self.input.length >= self.ok.length){
-			self.input = [];
-		}
-		*/
-
 	};
 
-	//the event listener
-	this.keyListener = function(){
-		window.onkeydown = function (e) {
-			//push the key pressed into the input array
-			self.input.push(e.keyCode);
+	//initiate input listener
+	window.addEventListener('keydown', check);
 
-			//call check function which returns true if both arrays are the same
-			if (self.check(self.input, self.ok)) {
-				self.input = [];
-				self.win();
-			}
-		};
+	//remove listener
+	this.stop = function(){
+		window.removeEventListener('keydown', check);
 	};
 
-	//launching the custom function
-	this.win = function(){
-		win_func();
-	};
-
-	//init the listener
-	return self.keyListener();
 }
